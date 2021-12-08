0decfdd9d0c3fcefe06d1e6a804b00b2d5f8b743
jogeyongsang <rudtkd134@naver.com>
2021-12-08 17:24:13 +0900

    library.py add

---
+++ library.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arrid=np.array([1,2,3,4])
+arrid
+
+arrid.shape
+
+arrN=np.arange(1,31).reshape(3,10)
+arrN
+
+display(arrN[1,5])
+
+display(arrN[2,-2])
+
+display(arrN[1,3:6])
+
+display(arrN[-2:,-3])
+
+display(arrN[:2,8:])
+
+arrn=np.arange(1,51).reshape(5,10)
+arrn
+
+display(arrn[1:4,3:7])
+
+display(arrn[1:4,1])
